bc3569a8acc28e5a2e201214af9ebfc59470c24a
Brice DUMONT <brice_dumont@outlook.fr>
2023-05-30 16:45:57 +0200

    Ajout quelques tests pokemons

--- tests/user.test.js
+++ tests/user.test.js
@@ -9,6 +9,7 @@ describe("tests pokemons", () => {
 
 	beforeAll(async () => {
 		await thisDb.sync({ force: true });
+        await createTrainer();
 	})
 
     afterAll(async () => {
@@ -29,10 +30,10 @@ describe("tests pokemons", () => {
         };
 	};
 
-    // const createTrainer = async () => {
-    //     const admin = await CreateTrainer('Leo', 'Pokemaniac', 'leopkmn', hashSync('cynthia', 10), '1999-10-09');
-    //     return admin;
-    // }
+    const createTrainer = async () => {
+        const admin = await CreateTrainer('Leo', 'Pokemaniac', 'leopkmn', 'cynthia', '1999-10-09');
+        return admin;
+    }
 
     test("it should pass", async () => {
         expect(true).toBe(true);
@@ -40,16 +41,26 @@ describe("tests pokemons", () => {
 
     describe("GET", () => {
         it ("Should return status code 400 with missing trainer", async () => {
-            const response = await request(app).get("/users/leopkmn/pokemons");
+            const response = await request(app).get("/users/random/pokemons");
             expect(response.statusCode).toBe(400);
+        });
+        it ("Should return status code 200 with existing trainer", async () => {
+            const response = await request(app).get("/users/leopkmn/pokemons");
+            expect(response.statusCode).toBe(200);
         });
     });
 
     describe("POST", () => {
         it ("Should return status code 400 with missing trainer", async () => {
 			let pokemon = getPokemon();
-			const response = await request(app).post("/users/leopkmn/pokemons").send(pokemon);
+			const response = await request(app).post("/users/random/pokemons").send(pokemon);
             expect(response.statusCode).toBe(400);
+            expect(typeof response.body).toBe('object');
+        });
+        it ("Should return status code 200 with existing trainer", async () => {
+			let pokemon = getPokemon();
+			const response = await request(app).post("/users/leopkmn/pokemons").send(pokemon);
+            expect(response.statusCode).toBe(200);
             expect(typeof response.body).toBe('object');
         });
     });
